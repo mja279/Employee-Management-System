93a321018cdd2af5de969bfe1bc1987ab44bd165
Maria Jimena <mja279@georgetown.edu>
2020-11-12 14:15:50 -0500

    pushing updates, not 100% running

--- seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Michael','Scott','Regional Manager','Management',70000, 'Self');
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Dwight', 'Schrute','RM Assistant','Sales',50000,'Michael Scott');
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Jim', 'Halpert','Salesman','Sales',52000,'Michael Scott');
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Pam', 'Beasley','Secretary','Administration',40750,'Michael Scott');
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Stanley', 'Hudson','Salesman','Sales',60000,'Michael Scott');
-INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES ('Phyllis', 'Lapin','Saleswoman','Sales',50000,'Michael Scott');
